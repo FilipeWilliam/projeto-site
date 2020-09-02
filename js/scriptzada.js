08aabe4cd864ef673f2b8ddd4fc53dd5d312c2b8
Filipe William <lipe.will@outlook.com>
2020-09-02 16:00:42 -0300

    fetchBitcoin refatorado

--- js/scriptzada.js
+++ js/scriptzada.js
@@ -8,7 +8,7 @@ import initDropDownMenu from './modules/dropDownMenu.js';
 import initMenuMobile from './modules/menuMobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -31,7 +31,6 @@ initScrollAnimation();
 initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
-
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
